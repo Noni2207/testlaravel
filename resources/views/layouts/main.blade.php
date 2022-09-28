9f98565dab30ad7a99ef59337f550b50068bd4a9
noni227 <81325180+Noni2207@users.noreply.github.com>
2022-09-28 22:30:33 +0200

    Build The Header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,9 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{(}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" 
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="{{('..css/styles.scss')}}">
 </head>
 <body>
-    
+    <header class="header">
+        <div class="conteiner">
+        <div class="header-logo">
+            Smith Reality
+        </div>
+        <div class="header-menu">
+            <a href="#" class="header__menu-link">Home</a>
+            <a href="#" class="header__menu-link">Listing</a>
+            <a href="#" class="header__menu-link">Property</a>
+            <a href="#" class="header__menu-link">Pages</a>
+        </div>
+        <div class="header-account">
+            <div class="header__account-link"><i class="fa-solid fa-heart"></i></div>
+            <div class="header__account-link"><i class="fa-solid fa-user"></i></div>
+        </div>
+    </div>
+    </header>
+@yield('content')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 // Show Home Page
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
 });
 // Show All Listing
 Route::get('/{property_type}/{listing_type}', function () {
